9a90c818f42b4c4ae87c504c81de5b96689af209
chooeeh <chchgml10@gmail.com>
2020-03-01 22:56:50 +0900

    2020/03/01 영화 설명 상세 페이지 뷰 xml 작성하기 (완료)

---
+++ app/src/main/java/com/example/prographytest/ui/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.prographytest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.prographytest.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
